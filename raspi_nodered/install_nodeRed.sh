a33dd68803998a19c1ac5699490d4ea48a62ddcc
Harald Pichler <harald@the-develop.net>
2015-02-26 13:58:00 +0100

    initial upload

---
+++ raspi_nodered/install_nodeRed.sh
@@ -0,0 +1,101 @@
+#!/bin/sh
+#$Id$
+
+updateApt()
+{
+	echo "\nUpdate Apt..."
+	read -p "apt-get update (j/n)? " update
+	case "$update" in
+				j)	sudo apt-get -y update
+					;;
+				n)	echo "Close..."
+					;;
+				*)	echo "false input"
+					;;
+			esac
+}
+
+upgradeApt()
+{
+	echo "\nUpgrade Apt..."
+	read -p "apt-get upgrade (j/n)? " upgrade
+	case "$upgrade" in
+				j)	sudo apt-get -y upgrade
+					;;
+				n)	echo "Close..."
+					;;
+				*)	echo "false input"
+					;;
+			esac
+}
+
+nodejs()
+{
+#	echo "\nInstall Node.js..."
+	read -p "\nDo you want to install Node.js (j/n)? " nodejs
+	case "$nodejs" in
+				j)	echo "\nInstall Node.js..."
+					sudo wget http://node-arm.herokuapp.com/node_latest_armhf.deb
+					sudo dpkg -i node_latest_armhf.deb
+					node -v
+					;;
+				n)	echo "Close..."
+					;;
+				*)	echo "false input"
+					;;
+			esac
+}
+
+nodeRed()
+{
+#	echo "\nExpand Filesystem..."
+	read -p "\nInstall Node-Red (j/n)? " nodeRed
+	case "$nodeRed" in
+				j)	echo "\nInstall Node-Red..."
+					sudo apt-get install git-core
+					git clone https://github.com/node-red/node-red.git
+					cd node-red
+					echo "\nInstall..."
+					sudo npm install
+					echo "\nInstall COAP- Node-Red..."
+					sudo npm install node-red-contrib-coap
+					echo "\nInstall COAP- Cli..."
+					sudo npm install coap-cli -g
+					;;
+				n)	echo "Close..."
+					;;
+				*)	echo "false input"
+					;;
+			esac
+	
+}
+
+
+
+
+read -p "Do you want to install Node-Red (j/n)? " response
+
+case "$response" in
+	j) 	echo "\nInstall Node-Red-Package..."
+		
+		dir1=`pwd`
+        #echo "dir1= " $dir1
+        dir2=`dirname $0`
+        #echo "dir2= " $dir2
+
+        cd $dir1
+        cd $dir2
+        #pwd
+
+		updateApt
+		upgradeApt
+		nodejs
+		nodeRed
+		sudo node red.js
+		;;
+	n)	echo "Close..."
+		;;
+	*)	echo "false input"
+		;;
+	
+esac
