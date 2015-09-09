8138b45a45924e6e5b7b46337d21f025e42dcefc
Harald Pichler <harald@the-develop.net>
2015-09-09 06:10:37 +0000

    remove sudo

--- banana-ajenti/banana-ajenti.sh
+++ banana-ajenti/banana-ajenti.sh
@@ -2,7 +2,7 @@
 wget -O- https://raw.github.com/ajenti/ajenti/1.x/scripts/install-debian.sh | sh
 # bugfix debian jessie gevent ssl
 service ajenti stop
-sudo apt-get install python-dev python-greenlet python-gevent python-vte python openssl python-crypto python-appindicator
-sudo apt-get install python-setuptools
+apt-get install python-dev python-greenlet python-gevent python-vte python openssl python-crypto python-appindicator
+apt-get install python-setuptools
 easy_install -U gevent
 service ajenti start
