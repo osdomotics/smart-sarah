ee693291f9e3b7c1e9fbcce7ed5cae94b907df70
Harald Pichler <harald@the-develop.net>
2015-11-03 08:45:18 +0100

    remove sudo

--- banana-omd/install_omd.sh
+++ banana-omd/install_omd.sh
@@ -29,9 +29,9 @@ coap()
 	read -p "Install COAP (y/n)? " coap
 	case "$coap" in
 				y)	echo "\nInstall coap..."
-					sudo apt-get -y install libnagios-plugin-perl libnetaddr-ip-perl 
+					apt-get -y install libnagios-plugin-perl libnetaddr-ip-perl 
 					echo "\n apt-get -f install"
-					sudo apt-get -f -y install
+					apt-get -f -y install
 					echo "\nWget..."
 					wget http://downloads.sourceforge.net/project/libcoap/coap-18/libcoap-4.0.3.tar.gz
 					echo "\nTar..."
@@ -39,7 +39,7 @@ coap()
 					echo "\ncd lib..."
 					cd libcoap-4.0.3
 					echo "\nInstall autoconf..."
-					sudo apt-get -y install autoconf
+					apt-get -y install autoconf
 					echo "\nAutoconf..."
 					autoconf
 					echo "\n./Configure..."
@@ -47,7 +47,7 @@ coap()
 					echo "\nMake..."
 					make
 					echo "\nCoap_client cp..."
-					sudo cp examples/coap-client /usr/local/bin/
+					cp examples/coap-client /usr/local/bin/
 					echo "\n Install COAP- END"
 					;;
 				n)	echo "Close..."
@@ -63,23 +63,23 @@ checkcoap()
 #	echo "\nExpand Filesystem..."
 	read -p "Install CheckCOAP (y/n)? " checkcoap
 	case "$checkcoap" in
-				y)	sudo omd create mysite
+				y)	omd create mysite
 	
 					cd $dir1
 					cd $dir2
 					
 					git clone https://github.com/osdomotics/check_coap.git
 					cd check_coap/
-					sudo cp check_coap* /omd/sites/mysite/lib/nagios/plugins/
+					cp check_coap* /omd/sites/mysite/lib/nagios/plugins/
 					cd nagios-conf.d/
-					sudo cp coap_templates.cfg /omd/sites/mysite/etc/nagios/conf.d/
-					sudo cp triops.cfg /omd/sites/mysite/etc/nagios/conf.d/	
+					cp coap_templates.cfg /omd/sites/mysite/etc/nagios/conf.d/
+					cp triops.cfg /omd/sites/mysite/etc/nagios/conf.d/	
 					
 					cd $dir1
 					cd $dir2
 					pwd
-					sudo cp conf.d/coap_templates.cfg /omd/sites/mysite/etc/nagios/conf.d/
-					sudo cp conf.d/triops.cfg /omd/sites/mysite/etc/nagios/conf.d/
+					cp conf.d/coap_templates.cfg /omd/sites/mysite/etc/nagios/conf.d/
+					cp conf.d/triops.cfg /omd/sites/mysite/etc/nagios/conf.d/
 					echo "\n Install CHECKCOAP- END"
 					;;
 				n)	echo "Close..."
@@ -108,7 +108,7 @@ case "$response" in
 		omd
 		coap
 		checkcoap
-		sudo omd restart
+		omd restart
 		;;
 	n)	echo "Close..."
 		;;
