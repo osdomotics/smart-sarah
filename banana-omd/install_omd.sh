6a687df9631f3c6105958f72e5eb41b708815e4b
Harald Pichler <harald@the-develop.net>
2016-11-30 10:11:27 +0100

    bugfix copy templates

--- banana-omd/install_omd.sh
+++ banana-omd/install_omd.sh
@@ -34,11 +34,9 @@ checkcoap()
 {
         omd create mysite
         git clone https://github.com/osdomotics/check_coap.git
-        cd check_coap/
-        cp check_coap* /omd/sites/mysite/lib/nagios/plugins/
-        cd nagios-conf.d/
-#        cp coap_templates.cfg /omd/sites/mysite/etc/nagios/conf.d/
-        cp sarah_hosts.cfg /omd/sites/mysite/etc/nagios/conf.d/
+        cp ./check_coap/check_coap* /omd/sites/mysite/lib/nagios/plugins/
+#        cp ./conf.d/coap_templates.cfg /omd/sites/mysite/etc/nagios/conf.d/
+        cp ./conf.d/sarah_hosts.cfg /omd/sites/mysite/etc/nagios/conf.d/
         cd $dir1
         cd $dir2
         echo "\n Install CHECKCOAP- END"
