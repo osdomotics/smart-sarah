17a2bc15e906409a51c71d95acedbdecd6244d41
Harald Pichler <harald@the-develop.net>
2017-10-24 11:31:20 +0200

    initial upload

---
+++ armhf/orangpizero/nodered/install_nodeRed.sh
@@ -0,0 +1,75 @@
+#!/bin/sh
+#$Id$
+
+updateApt()
+{
+	apt-get -y update
+}
+
+upgradeApt()
+{
+	apt-get -y upgrade
+}
+
+nodejs()
+{
+
+        apt-get -y install nodejs npm
+        npm cache clean -f
+        npm install -g n
+        n stable
+        npm install -g npm@3.x
+}
+
+nodeRed()
+{
+					echo "\nInstall..."
+					apt-get -y install mongodb-server
+				        npm cache clean
+					echo "\nInstall COAP- Cli..."
+					npm install coap-cli -g
+					npm install redis -g
+					npm install mongodb -g
+                                        echo "\nInstall Node-Red..."
+					npm install -g --unsafe-perm node-red
+					echo "\nInstall COAP- Node-Red..."
+					npm install -g node-red-contrib-coap
+					echo "\nInstall mongodb2- Node-Red..."
+					npm install -g node-red-contrib-mongodb2
+					echo "\nInstall UI-Dashboard Node-Red..."
+					npm install -g node-red-dashboard
+					echo "\n Install Start Script"
+					npm install -g pm2
+                                        pm2 start /usr/local/bin/node-red --node-args="--max-old-space-size=128" -- -v
+                                        pm2 save
+                                        pm2 startup systemd
+}
+
+
+
+
+read -p "Do you want to install Node-Red (y/n)? " response
+
+case "$response" in
+	y) 	echo "\nInstall Node-Red-Package..."
+		
+		dir1=`pwd`
+        #echo "dir1= " $dir1
+        dir2=`dirname $0`
+        #echo "dir2= " $dir2
+
+        cd $dir1
+        cd $dir2
+        #pwd
+
+		updateApt
+		upgradeApt
+		nodejs
+		nodeRed
+		;;
+	n)	echo "Close..."
+		;;
+	*)	echo "false input"
+		;;
+	
+esac
